7b15d56c60dba89effa01a17536a16c0f5d3a4cb
ame <ortegax102938@gmail.com>
2022-09-07 14:46:12 +0800

    First Commit

---
+++ JSsample.js
@@ -0,0 +1,6 @@
+    console.log("-------------------------");
+console.log("Rise & Shine!");
+console.log("Ready for a new day");
+console.log("-----------------------")
+console.log ("how are you today")
+console.log ("im good thanks how about you?")
