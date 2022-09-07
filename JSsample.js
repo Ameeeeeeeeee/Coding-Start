2342db052b011624bb7f0fac0d347e6618872e9d
ame <ortegax102938@gmail.com>
2022-09-07 14:54:37 +0800

    Added Features

--- JSsample.js
+++ JSsample.js
@@ -4,3 +4,4 @@ console.log("Ready for a new day");
 console.log("-----------------------")
 console.log ("how are you today")
 console.log ("im good thanks how about you?")
+<strong>Warning!</strong> Represents a warning that needs attention.</br>
---
+++ phytonsample.py
@@ -0,0 +1,4 @@
+print("-------------------")
+print("Phyton has entered the building.")
+print("Let the party start!")
+print("--------------------------")
